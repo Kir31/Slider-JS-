3605b6112cb535fdea5df5f404b1cd48f3419fe8
Kir31 <dak3103@gmail.com>
2022-05-12 19:19:03 +0300

    feat: create slide animation, controls (Thu, May 12, 2022 07:19:03 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,9 +1,13 @@
 const slides = document.querySelector('.slides');
 const images = document.querySelectorAll('img');
 const slidesWrapper = document.querySelector('.wrapper-slides');
-let slideWidth;
+const controls  = document.querySelector('.controls')
 
+let slideWidth;
 let slideIndex = 0;
+let offset = 0;
+
+// const textArr = ['Helsingborg, Sweden. July 2016', 'Copenhagen, Denmark. July 2016', 'Italy. August 2017', 'Provance, France. August 2017', 'Menton, France. August 2017']
 
 function loadSlider() {
     slideWidth = window.getComputedStyle(slidesWrapper).width;
@@ -17,3 +21,23 @@ function loadSlider() {
 window.addEventListener('resize', loadSlider);
 
 loadSlider();
+
+const dots = []; 
+for (let i = 0; i < images.length; i++) {
+    const item = document.createElement('li');
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    item.setAttribute('data-slide', i);
+    controls.append(item);
+    item.append(dot);    
+    dots.push(item);
+}
+
+dots.forEach(dot => dot.addEventListener('click', () => {
+    const slide = dot.getAttribute('data-slide');
+    slideIndex = slide;
+    offset = +slideWidth.slice(0, slideWidth.length - 2) * slide;
+    slides.style.transform = `translateX(-${offset}px)`;
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[slideIndex].classList.add('active');  
+}))
